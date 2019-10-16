dcc99fc69eb08a01e1a6fbeba3841b0af605fd50
Yash Rathore <yash@vitwit.com>
2019-10-16 13:19:16 +0530

    update pkg in generated sh, updated package

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,14 @@ import { parseArgumentsIntoOptions, Schedular } from "@vitwit/js-sdkgen";
 import { CodePlusDocGenPlusRedux } from "./main";
 
 export async function cli(args) {
-  const options = parseArgumentsIntoOptions(args);
+  const { parsedArgs, ...options } = parseArgumentsIntoOptions(args);
+
   if (!options.jsonFile) {
     console.error("%s --json-file is required", chalk.red.bold("ERROR"));
 
     process.exit(1);
   }
-  const schedular = new Schedular(options,{pkg:"redux envelope"});
+  const schedular = new Schedular(options,parsedArgs, { pkg: "redux-envelope" });
   schedular.CodeGenToUse = CodePlusDocGenPlusRedux;
   await schedular.generateSDK();
 }
